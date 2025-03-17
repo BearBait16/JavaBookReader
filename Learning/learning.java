ac62eab170568cc5bf556c6597e26e327d448cc7
Jackson Aden <jacksonaden@gmail.com>
2025-03-17 08:12:44 -0600

    Started the Repo, did some super basic java learning

---
+++ Learning/learning.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String args[]) {
+        System.out.println(3 + 3);
+        String name = "Jackson";
+        System.out.println(name);
+        int num = 77;
+        System.out.println(num);
+    }
+}
