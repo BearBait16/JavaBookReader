771f2e19f8762dc5dd2cd3cba8969d1dc2a0ccb3
Jackson Aden <jacksonaden@gmail.com>
2025-03-21 20:28:20 -0600

    Added the text file, and the begginings of my Ollama caller.

---
+++ BookReader/aiReader.java
@@ -0,0 +1,70 @@
+package JavaBookReader.BookReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.net.http.HttpClient;
+
+public class aiReader {
+    public static void main(String[] args) {
+        bomReader();
+        String model = "deepseek-r1:14b";
+        System.out.println("What is your input? ");
+        Scanner scanner = new Scanner(System.in);
+        String prompt = scanner.nextLine();
+        scanner.close();
+        Thread ollamaThread = new Thread(() -> {
+            genericOllamaRequest(model, prompt);
+        });
+        ollamaThread.start();
+    }
+
+    static void bomReader() {
+        try {
+            File bookOfMormon = new File("JavaBookReader\\BookReader\\Book_of_Mormon.txt");
+            Scanner reader = new Scanner(bookOfMormon);
+            System.out.println("Book of Mormon");
+            for (int i = 0; i <= 10; i++) {
+                String line = reader.nextLine();
+                ArrayList<String> verse = new ArrayList<>();
+                if (line != "\n") {
+                    verse.add(line);
+                }
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Your code made a stinky");
+            e.printStackTrace();
+        }
+    }
+
+    public static void genericOllamaRequest(String model, String prompt) {
+        String api = "http://localhost:11434/api/generate";
+        String jsonBody = String.format("""
+                {
+                    "mode": "%s"
+                    "prompt": "%s"
+                }
+                                    """, model, prompt);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(api))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody, StandardCharsets.UTF_8))
+                .build();
+        ;
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Your Prompt was: " + response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
